00bfc097863c07bff18b452b46e3c860d2a0b3e3
Samuel <samuelcruz9p@gmail.com>
2025-02-25 20:28:07 +0000

    ideas

--- vending/main.py
+++ vending/main.py
@@ -1,5 +1,22 @@
 def run(input_file: str, output_file: str) -> None:
-    # TODO
+    with open(input_file) as operations:
+        operations_list = operations.readlines()
+
+
+maquina = {'balance': 0, 'products': {}}
+
+
+def money_sum(op_list: list):
+    money_counter = 0
+    if op_list[0] == 'M':
+        money_counter += op_list[1]
+        return money_counter
+
+
+def change_price(op_list: list):
+    if op_list[0] == 'P':
+        product = op_list[1]
+        maquina['products'][product]
 
 
 # DO NOT TOUCH THE CODE BELOW
