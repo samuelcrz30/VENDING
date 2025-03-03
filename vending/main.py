3bc7a5f7a147fcf6c74c1a67658b94875ba6172b
Samuel <samuelcruz9p@gmail.com>
2025-03-03 22:24:02 +0000

    arreglo funciones 1

--- vending/main.py
+++ vending/main.py
@@ -1,41 +1,40 @@
+def order_product(machine: dict, product, quantity, money):
+    machine['products'][product]['stock'] -= quantity
+    machine['balance'] += money
+    machine['balance'] -= money % ((machine['products'][product]['price']) * (quantity))
+
+
+def replace_product(machine: dict, product, pro_stock):
+    if product not in machine['products']:
+        machine['products'] = product
+        machine['products'][pro_stock]['stock'] = pro_stock
+        machine['products'][product]['price'] = 1
+    else:
+        machine['products'][pro_stock]['stock'] += pro_stock
+
+
+def change_price(machine: dict, product, price):
+    machine['products'][product] = int(price)
+
+
+def push_money(machine: dict, money: str):
+    machine['balance'] += money
+
+
 def run(input_file: str, output_file: str) -> None:
-    vending_machine = {
-        'balance': 0,
-        'products': {
-            '': {'stock': 0, 'price': 0},
-        },
-    }
+    vending_machine = {'balance': 0, 'products': {'': {'stock': 0, 'price': 0}}}
     with open(input_file, 'r') as orders_data:
         product_info = orders_data.readline().split()
         order_code = product_info[0]
         match order_code:
             case 'O':
-                product = product_info[1]
-                quantity_requested = int(product_info[2])
-                money_to_pay = int(product_info[3])
-                vending_machine['products'][product]['stock'] -= quantity_requested
-                vending_machine['balance'] += money_to_pay
-                vending_machine['balance'] -= money_to_pay % (
-                    (vending_machine['products'][product]['price']) * (quantity_requested)
-                )
-
+                order_product(vending_machine, product_info[1], product_info[2], product_info[3])
             case 'R':
-                product = product_info[1]
-                product_stock = int(product_info[2])
-                if product not in vending_machine['products']:
-                    vending_machine['products'] = product
-                    vending_machine['products'][product_stock]['stock'] = product_stock
-                    vending_machine['products'][product]['price'] = 1
-                else:
-                    vending_machine['products'][product_stock]['stock'] += product_stock
-
+                replace_product(vending_machine, product_info[1], product_info[2])
             case 'P':
-                product = product_info[1]
-                price = product_info[2]
-                vending_machine['products'][product] = int(price)
+                change_price(vending_machine, product_info[1], product_info[2])
             case 'M':
-                money_to_restore = product_info[1]
-                vending_machine['balance'] += money_to_restore
+                push_money(vending_machine, product_info[1])
     with open(output_file, 'w') as after_orders:
         after_orders.write(f'{vending_machine["balance"]}')
 
